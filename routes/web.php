24a2130dea98a270cd70e93d460eef62bb1e3fd3
danial <danialdark12098@gmail.com>
2021-11-12 15:56:11 +0330

    adding admin prefix route

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+route::prefix("admin")->middleware(["auth", "admin"])->group(function () {
+    
+});
