ea1aa830f2b3659da10571027fe3760960f6d51b
danial <danialdark12098@gmail.com>
2021-11-12 15:52:27 +0330

    making middleware to check if use is admin or not

---
+++ app/Http/Middleware/AdminCheck.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AdminCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // getting user roles
+        $roles = array();
+        foreach (auth()->user()->roles as $role) {
+            array_push($roles, $role->name);
+        }
+        // getting user roles
+
+        // check if user is super admin or not
+        if (in_array("superadmin", $roles)) {
+            return $next($request);
+        } else {
+            return redirect()->route("home.index");
+        }
+    }
+}
