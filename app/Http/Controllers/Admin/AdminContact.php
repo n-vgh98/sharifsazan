36e5f97def7524b7af67c494c2f8a7a3aee33555
danial <danialdark12098@gmail.com>
2021-11-13 12:29:28 +0330

    completing contact us page for admin panel

--- app/Http/Controllers/Admin/AdminContact.php
+++ app/Http/Controllers/Admin/AdminContact.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Contact_Us;
 use Illuminate\Http\Request;
 
 class AdminContact extends Controller
@@ -14,7 +15,8 @@ class AdminContact extends Controller
      */
     public function index()
     {
-        return view("admin.contact.index");
+        $contacts = Contact_Us::all();
+        return view("admin.contact.index", compact("contacts"));
     }
 
     /**
--- resources/views/admin/contact/index.blade.php
+++ resources/views/admin/contact/index.blade.php
@@ -3,19 +3,70 @@
     <table class="table table-striped">
         <thead>
             <tr>
+                <th scope="col"></th>
+                <th scope="col">فایل ارسالی</th>
+                <th scope="col">خلاصه متن</th>
+                <th scope="col">ایمیل یا شماره تلفن</th>
+                <th scope="col">نام ارسال کننده</th>
                 <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
+            {{-- make number for rows --}}
+            @php
+                $number = 1;
+            @endphp
+
+            {{-- get each contact_us message datas --}}
+            @foreach ($contacts as $contact)
+                <tr>
+                    <td>
+                        <button type="button" class="btn btn-primary" data-toggle="modal"
+                            data-target="#text{{ $contact->id }}">
+                            مشاهده متن کامل
+                        </button>
+                    </td>
+
+                    <td>
+                        <a href="#" class="btn btn-primary">دانلود فایل</a>
+                    </td>
+
+                    <td>{{ substr($contact->text, 0, 25) }}</td>
+
+                    <td>{{ $contact->email }}</td>
+
+                    <td>{{ $contact->name }}</td>
+
+                    <th scope="row">{{ $number }}</th>
+                </tr>
+
+                {{-- modal for show full messsage --}}
+                <div class="modal fade" id="text{{ $contact->id }}" tabindex="-1" role="dialog"
+                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">متن پیام</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p>{{ $contact->text }}</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">دیدم</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {{-- make number for rows --}}
+                @php
+                    $number++;
+                @endphp
+            @endforeach
+
         </tbody>
     </table>
 @endsection
