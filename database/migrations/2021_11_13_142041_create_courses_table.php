372956192d953592b1746fa0cdab08bae0417601
danial <danialdark12098@gmail.com>
2021-11-13 18:46:43 +0330

    making courses migration

---
+++ database/migrations/2021_11_13_142041_create_courses_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->text("introduction");
+            $table->text("description");
+            $table->decimal("price", 20, 2);
+            $table->tinyInteger("licensable")->comment("1 is ye and 0 is no");
+            $table->string("master_name");
+            $table->string("master_job");
+            $table->tinyInteger("type")->comment("1 is physical and 0 is electronic");
+            $table->tinyInteger("mode")->comment("1 is ye and 0 is no");
+            $table->text("link")->nullable()->comment("for electronic courses");
+            $table->text("introduction_v_link")->comment("for introduction video");
+            $table->integer("off")->default(0)->comment("takhfifat be darsad");
+            $table->bigInteger("use_time")->default(0)->comment("tedad dafat estefade baraye tain mizane mahboodiat");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
