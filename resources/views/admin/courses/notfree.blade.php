24657722dd6c4d1eabc831392df24fbaa5bcd84e
danial <danialdark12098@gmail.com>
2021-11-13 20:42:18 +0330

    changing other pages(removing modals and linking them to create and edit page

--- resources/views/admin/courses/notfree.blade.php
+++ resources/views/admin/courses/notfree.blade.php
@@ -1,10 +1,10 @@
 @extends('admin.layouts.master')
 @section('sitetitle')
-    دوره های پولی
+    تمام دوره ها
 @endsection
 
 @section('pagetitle')
-    دوره های پولی
+    تمام دوره ها
 @endsection
 @section('content')
     <table class="table  table-striped">
@@ -64,33 +64,11 @@
                     <th scope="row">{{ $number }}</th>
                 </tr>
 
-                {{-- modal for editing courses --}}
-                <div class="modal fade" id="edit{{ $course->id }}" tabindex="-1" role="dialog"
-                    aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                ...
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {{-- end ofmodal for editing courses --}}
-
                 @php
                     $number++;
                 @endphp
             @endforeach
         </tbody>
     </table>
+    <a href="{{ route('admin.courses.create') }} " class="btn btn-primary">ساخت دوره جدید</a>
 @endsection
