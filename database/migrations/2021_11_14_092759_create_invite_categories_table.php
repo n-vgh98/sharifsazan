2834803a03d2d6d6759ef262f9f2d67fe13eefcb
danial <danialdark12098@gmail.com>
2021-11-14 13:09:16 +0330

    adding migration and models for invite section

---
+++ database/migrations/2021_11_14_092759_create_invite_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInviteCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invite_categories', function (Blueprint $table) {
+            $table->id();
+            $table->text("register_form_link");
+            $table->text("technical_exam_form_link")->comment("fani");
+            $table->text("practical_exam_form_link")->comment("amali");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invite_categories');
+    }
+}
---
+++ database/migrations/2021_11_14_092950_create_invite_pages_category.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvitePagesCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invite_pages_category', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger("title")->comment("0 is sabte nam  1 is azmoon fani 2 is azmoon amali");
+            $table->foreignId("category_id")->constrained("invite_categories")->onDelete("cascade")->onUpdate("cascade");
+            $table->text("text1");
+            $table->text("text2")->nullable()->comment("baraye vaghti hast ke mikhan dar do ghesmate matn dashte bashan");
+            $table->text("image");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invite_pages_category');
+    }
+}
