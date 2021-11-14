3031383b9637b9ac03babe848fd550ec50ce4b71
danial <danialdark12098@gmail.com>
2021-11-15 01:52:34 +0330

    changing migration and model to image instead of photo (bara rabete polumorphic in esme be nazaram behtar bood

--- database/migrations/2021_11_14_222118_create_images_table.php
+++ database/migrations/2021_11_14_222118_create_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePhotosTable extends Migration
+class CreateImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePhotosTable extends Migration
      */
     public function up()
     {
-        Schema::create('photos', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->string("name");
             $table->text("path");
@@ -30,6 +30,6 @@ class CreatePhotosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('photos');
+        Schema::dropIfExists('images');
     }
 }
