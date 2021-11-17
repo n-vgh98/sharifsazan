221e57f9e72a41e01513e19c9f2543f3431a812c
danial <danialdark12098@gmail.com>
2021-11-17 14:05:08 +0330

    showing name of user on panels on top right

--- resources/views/writer/layouts/header.blade.php
+++ resources/views/writer/layouts/header.blade.php
@@ -233,7 +233,7 @@
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                        <span class="hidden-xs">شریف سازان</span>
+                        <span class="hidden-xs">{{ auth()->user()->name }}</span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- User image -->
