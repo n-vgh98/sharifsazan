65fd3b5d40047d8fc0ae5b6d2cfde66c2432bf20
danial <danialdark12098@gmail.com>
2021-11-19 17:25:41 +0330

    making migration and model for courses

---
+++ database/migrations/2021_11_19_135354_create_english_courses_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnglishCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('english_courses', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->text("introduction");
+            $table->text("description");
+            $table->text("meta_key_words")->nullable();
+            $table->text("meta_descriptions")->nullable();
+            $table->decimal("price", 20, 0);
+            $table->tinyInteger("licensable")->comment("1 is ye and 0 is no");
+            $table->string("master_name");
+            $table->string("master_job");
+            $table->tinyInteger("type")->comment("1 is physical and 0 is electronic");
+            $table->tinyInteger("mode")->comment("1 is online and 0 is offline");
+            $table->text("link")->nullable()->comment("for electronic courses");
+            $table->text("introduction_v_link")->comment("for introduction video");
+            $table->integer("off")->default(0)->comment("takhfifat be darsad");
+            $table->bigInteger("use_time")->default(0)->comment("tedad dafat estefade baraye tain mizane mahboodiat");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('english_courses');
+    }
+}
