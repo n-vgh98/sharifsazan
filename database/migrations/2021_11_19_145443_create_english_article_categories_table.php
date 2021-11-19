2453cb018ce1075f3421257cae5d723566850175
danial <danialdark12098@gmail.com>
2021-11-19 18:26:54 +0330

    making model and migration for article categories and articles

--- database/migrations/2021_11_19_145443_create_english_article_categories_table.php
+++ database/migrations/2021_11_19_145443_create_english_article_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticlesTable extends Migration
+class CreateEnglishArticleCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
+        Schema::create('english_article_categories', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("category_id")->constrained("article_categories")->onDelete("cascade")->onUpdate("cascade");
             $table->string("title");
-            $table->text("text");
-            $table->text("meta_key_words")->nullable();
-            $table->text("meta_descriptions")->nullable();
             $table->timestamps();
         });
     }
@@ -31,6 +27,6 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('articles');
+        Schema::dropIfExists('english_article_categories');
     }
 }
--- database/migrations/2021_11_19_145455_create_english_articles_table.php
+++ database/migrations/2021_11_19_145455_create_english_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticlesTable extends Migration
+class CreateEnglishArticlesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
+        Schema::create('english_articles', function (Blueprint $table) {
             $table->id();
             $table->foreignId("category_id")->constrained("article_categories")->onDelete("cascade")->onUpdate("cascade");
             $table->string("title");
@@ -31,6 +31,6 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('articles');
+        Schema::dropIfExists('english_articles');
     }
 }
