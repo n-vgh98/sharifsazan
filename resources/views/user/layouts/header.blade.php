ad3056a115b2b4060455328416798ef344d6c14e
Danial <danialdark12098@gmail.com>
2022-01-16 23:04:00 +0330

    fixing bug for changing language from menu

--- resources/views/user/layouts/header.blade.php
+++ resources/views/user/layouts/header.blade.php
@@ -2,240 +2,8 @@
  @php
 
      $x = substr(Request::getPathInfo(), 3);
-     $lang = substr(Request::getPathInfo(), 1, 2);
-     if ($lang == 'fa') {
-         $lang = 'en';
-         $link = $lang . $x;
-     } else {
-         $lang = 'fa';
-         $link = $lang . $x;
-     }
-
  @endphp
- {{-- <menu class="parent-responsive-menu">
 
-     <div class="wrapper-responsive-menu">
-         <!-- flag nav start -->
-         <nav class="nav-flag">
-             <ul class="ul-flag">
-                 <li>
-                     <span>
-                         <i class="fas fa-chevron-down"></i>
-                     </span>
-                     <span>fa</span>
-                     <img src="{{ asset('frontend/imgs/iran-flag.png') }}" alt="">
-                     <ul class="wrapper-flag">
-                         <span>
-                         </span>
-                         <span>eg</span>
-                         <img src="{{ asset('frontend/imgs/eng-flag.png') }}" alt="">
-                     </ul>
-                 </li>
-             </ul>
-         </nav>
-         <!-- flag nav end -->
-         <!-- left nav start -->
-         <nav class="left-nav-responsive-menu">
-             <ul class="left-ul-responsive-menu">
-                 <li>
-                     <i class="far fa-comment-alt"></i>
-                 </li>
-                 <li>
-                     <i class="fas fa-shopping-cart"></i>
-                 </li>
-                 <button class="btn-responsive-menu">
-                     <i class="fas fa-bars"></i>
-
-                 </button>
-             </ul>
-
-
-         </nav>
-         <!-- left nav start -->
-     </div>
-
-     <!-- nav-flag-main-menu" end -->
-     <!-- start main menu  -->
-
-
-     <nav class="nav-main-menu">
-         <ul class="ul-main-menu">
-             <!-- nav-flag-main-menu" start -->
-             <nav class="nav-flag-main-menu">
-                 <ul class="ul-flag-main-menu">
-                     <li>
-                         <span>
-                             <i class="fas fa-chevron-down"></i>
-                         </span>
-                         <span>fa</span>
-                         <img src="{{ asset('frontend/imgs/iran-flag.png') }}" alt="">
-                         <ul class="wrapper-flag-main-menu">
-                             <span>
-                             </span>
-                             <span>eg</span>
-                             <img src="{{ asset('frontend/imgs/eng-flag.png') }}" alt="">
-                         </ul>
-                     </li>
-                 </ul>
-             </nav>
-             <!-- nav-flag-main-menu" end -->
-             <li>
-                 <a href="{{ route('home') }}">خانه</a>
-             </li>
-             <li>
-
-                 خدمات
-
-                 <i class="fa fa-chevron-down"></i>
-                 <!-- start wrapper-main-menu خدمات -->
-                 <ul class="wrapper-main-menu">
-                     <li>
-                         <a href="khadamat.html">توجیه طرح بر مبنای اقتصاد</a>
-                     </li>
-                     <li>
-                         <a href="khadamat.html">
-                             طراحی و شبیه سازی
-                         </a>
-                     </li>
-                     <li>
-                         <a href="khadamat.html">
-                             طرح توجیهی مجتمع های صنعتی
-                         </a>
-                     </li>
-
-                     <li>
-                         <a href="khadamat.html">
-                             بهینه سازی و مقاوم سازی
-                         </a>
-                     </li>
-                 </ul>
-                 <!-- end wrapper-main-menu خدمات -->
-             </li>
-             <li>
-
-                 تحقیق توسعه
-
-                 <i class="fa fa-chevron-down"></i>
-                 <!-- start wrapper-main-menu تحقیق توسعه-->
-                 <ul class="wrapper-main-menu">
-                     <li>
-                         <a href="amozesh.html">
-                             دوره اموزشی
-                         </a>
-                     </li>
-                     @php
-                         $articleCategoryLanguages = App\Models\Lang::where([['name', $lang], ['langable_type', 'App\Models\ArticleCategory']])->get();
-                     @endphp
-                     @foreach ($articleCategoryLanguages as $articleCategoryLanguage)
-                         @php
-                             $articleCategory = $articleCategoryLanguage->langable;
-                         @endphp
-                         <li>
-                             <a href="#">
-                                 {{ $articleCategory->title }}
-
-                             </a>
-                         </li>
-                     @endforeach
-                     <li>
-                         <a href="article.html">
-                             کتابخانه الکترونیکی
-                         </a>
-                     </li>
-                 </ul>
-                 <!-- end wrapper-main-menu تحقیق توسعه-->
-             </li>
-             <li>
-                 دعوت به همکاری
-                 <i class="fa fa-chevron-down"></i>
-                 <!-- start wrapper-main-menu دعوت به همکاری-->
-                 <ul class="wrapper-main-menu">
-                     <li>
-                         <a href="Davatbehamkari.html">
-                             واحد سازه و پل
-                         </a>
-                     </li>
-                     <li>
-                         <a href="Davatbehamkari.html">
-                             طراحی و شبیه سازی
-                         </a>
-                     </li>
-                     <li>
-                         <a href="Davatbehamkari.html">
-                             واحد معماری و طراحی داخلی واحد اداری-مالی
-                         </a>
-                     </li>
-
-                     <li>
-                         <a href="Davatbehamkari.html">
-                             واحد بازاریابی و فروش
-                         </a>
-                     </li>
-                 </ul>
-                 <!-- end wrapper-main-menu دعوت به همکاری-->
-
-             </li>
-             <li><a href="Proje.html">پروژها</a></li>
-             <li><a href="tamasbama.html">تماس با ما</a></li>
-             <li><a href="TeamMa.html">درباره ما</a></li>
-
-         </ul>
-
-
-     </nav>
-
-
-     <!-- start icon main -->
-     <nav class="nav-icon-main">
-         <ul class="ul-icon-main">
-             <li>
-                 <a href="#">
-                     <i class="fas fa-comment"></i>
-                 </a>
-             </li>
-             <li>
-                 <a href="#">
-                     <i class="fa fa-shopping-cart"></i>
-                 </a>
-             </li>
-             <li>
-                 @if (Auth::check())
-                     <a href="{{ route('panel.index') }}"><i
-                             class="fas">{{ auth()->user()->name }}</i></a>
-                 @else
-                     <a href="{{ route('login') }}">
-                         <i class="fas fa-user"></i>
-                     </a>
-                 @endif
-
-                 <ul class="wrapper-user-icon">
-                     <li class="setting-menu">
-                         <p> تنظیمات</p>
-                         <i class="fas fa-cog"></i>
-
-                     </li>
-                     <li class="settingg-menu">
-
-                         <p> خروج </p>
-                         <i class="fas fa-power-off"></i>
-                     </li>
-                 </ul>
-             </li>
-         </ul>
-         @auth
-
-             <form action="{{ route('logout') }}" method="POST">
-                 @csrf
-                 <button type="submit">خروج</button>
-             </form>
-         @endauth
-
-     </nav>
-     <!-- end icon main -->
-
-
-     <!-- end main menu  -->
- </menu> --}}
 
 
  <!-- main responsive top menu static start -->
@@ -250,28 +18,39 @@
                  <i class="fas fa-chevron-down"></i>
              </p>
              <div class="menu-flag2">
-                 <ul>
+                <ul>
 
-                     @if ($lang == 'fa')
-                         <li><a href="/{{ $link }}">Fa</a><img
-                                 src="{{ asset('frontend/imgs/iran-flag.png') }}" alt="">
-                             </a>
-                         </li>
-                         <hr>
-                         <li><a href="#">En</a><img src="{{ asset('frontend/imgs/eng-flag.png') }}" alt="">
-                             </a>
-                         </li>
-                     @else
-                         <li><a href="#">Fa</a><img src="{{ asset('frontend/imgs/iran-flag.png') }}" alt="">
-                             </a>
-                         </li>
-                         <hr>
-                         <li><a href="/{{ $link }}">En</a><img src="{{ asset('frontend/imgs/eng-flag.png') }}"
-                                 alt="">
-                             </a>
-                         </li>
-                     @endif
-                 </ul>
+                    @php
+                        $lang = substr(Request::getPathInfo(), 1, 2);
+                        if ($lang == 'fa') {
+                            $lang = 'en';
+                            $link = $lang . $x;
+                        } else {
+                            $lang = 'fa';
+                            $link = $lang . $x;
+                        }
+                    @endphp
+
+                    @if ($lang == 'en')
+                        <li><a href="#">Fa</a><img src="{{ asset('frontend/imgs/iran-flag.png') }}" alt="">
+                            </a>
+                        </li>
+                        <hr>
+                        <li><a href="/{{ $link }}">En</a><img
+                                src="{{ asset('frontend/imgs/eng-flag.png') }}" alt="">
+                            </a>
+                        </li>
+                    @else
+                        <li><a href="/{{ $link }}">Fa</a><img
+                                src="{{ asset('frontend/imgs/iran-flag.png') }}" alt="">
+                            </a>
+                        </li>
+                        <hr>
+                        <li><a href="#">En</a><img src="{{ asset('frontend/imgs/eng-flag.png') }}" alt="">
+                            </a>
+                        </li>
+                    @endif
+                </ul>
 
 
              </div>
@@ -357,7 +136,9 @@
                              دوره اموزشی
                          </a>
                      </li>
+
                      @php
+                         $lang = substr(Request::getPathInfo(), 1, 2);
                          $articleCategoryLanguages = App\Models\Lang::where([['name', $lang], ['langable_type', 'App\Models\ArticleCategory']])->get();
                      @endphp
                      @foreach ($articleCategoryLanguages as $articleCategoryLanguage)
@@ -423,22 +204,33 @@
              <div class="menu-flag2">
                  <ul>
 
-                     @if ($lang == 'fa')
-                         <li><a href="/{{ $link }}">Fa</a><img
-                                 src="{{ asset('frontend/imgs/iran-flag.png') }}" alt="">
-                             </a>
-                         </li>
-                         <hr>
-                         <li><a href="#">En</a><img src="{{ asset('frontend/imgs/eng-flag.png') }}" alt="">
-                             </a>
-                         </li>
-                     @else
+                     @php
+                         $lang = substr(Request::getPathInfo(), 1, 2);
+                         if ($lang == 'fa') {
+                             $lang = 'en';
+                             $link = $lang . $x;
+                         } else {
+                             $lang = 'fa';
+                             $link = $lang . $x;
+                         }
+                     @endphp
+
+                     @if ($lang == 'en')
                          <li><a href="#">Fa</a><img src="{{ asset('frontend/imgs/iran-flag.png') }}" alt="">
                              </a>
                          </li>
                          <hr>
                          <li><a href="/{{ $link }}">En</a><img
                                  src="{{ asset('frontend/imgs/eng-flag.png') }}" alt="">
+                             </a>
+                         </li>
+                     @else
+                         <li><a href="/{{ $link }}">Fa</a><img
+                                 src="{{ asset('frontend/imgs/iran-flag.png') }}" alt="">
+                             </a>
+                         </li>
+                         <hr>
+                         <li><a href="#">En</a><img src="{{ asset('frontend/imgs/eng-flag.png') }}" alt="">
                              </a>
                          </li>
                      @endif
