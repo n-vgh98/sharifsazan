d6d4fa9b7cb154634a946428b114fca548d54569
n-vgh98 <nazi.gh988@gmail.com>
2022-01-18 00:17:24 +0330

    create service and service categories migartion and model

---
+++ database/migrations/2022_01_17_202116_create_service_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->text('meta_keywords');
+            $table->text('meta_description');
+            $table->foreignId('category_id')->constrained('service_categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('service');
+    }
+}
